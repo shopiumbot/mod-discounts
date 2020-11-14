7e4baecc37120b59e31e7260bdccee4de8e35dba
Andrew <andrew.panix@gmail.com>
2020-11-14 10:26:21 +0200

    ``

---
+++ messages/en/Discount.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'NAME' => 'Name',
+    'SUM' => 'Discount',
+    'START_DATE' => 'Start date',
+    'END_DATE' => 'End date',
+    'SWITCH' => 'Status',
+    'MANUFACTURERS' => 'Manufacturers',
+    'USER_ROLES' => 'User Groups',
+    'HINT_SUM' => 'Please enter an integer or percentage. For example 10%. ',
+    'HINT_MANUFACTURERS' => 'Select manufacturer',
+    'HINT_USER_ROLES' => '<strong>Warning:</strong> Discounts for admins are not allowed.',
+    'TAB_CATEGORIES' => 'Categories',
+    'TAB_MAIN' => 'Main'
+];
---
+++ messages/en/default.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'MODULE_NAME' => 'Discounts',
+    'MODULE_DESC' => 'Manage store discounts',
+    'CREATE_DISCOUNT' => 'Add discount',
+    'CATEGORY_INFO'=>'Here you can specify the categories for which the discount will be available.'
+];
